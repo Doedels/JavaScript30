5483273a0d69ebbac0b647277ee905c0062ab85c
Doedels <sandermarkies@yahoo.com>
2020-09-30 19:11:42 +0200

    finished it

--- 01 - Drum Kit/script.js
+++ 01 - Drum Kit/script.js
@@ -1,8 +1,23 @@
-window.addEventListener("keydown", (e) => {
+const playSound = (e) => {
     const audio = document.querySelector(`audio[data-key=${e.key}]`);
     const key = document.querySelector(`.key[data-key=${e.key}]`);
     if (!audio) return; // stop the function from running
-    audio.currentTime = 0 // reset possible running audio
+    audio.currentTime = 0;// reset possible running audio
     audio.play();
-    console.log(key);
+    key.classList.add("playing");
+}
+
+const removeTransition = (e) => {
+    if (e.propertyName !== "transform") return; // skip if its not a transform
+    e.target.classList.remove("playing");
+}
+
+window.addEventListener("keydown", playSound);
+
+const keys = document.querySelectorAll(`.key`);
+
+keys.forEach(key => {
+    key.addEventListener("transitionend", removeTransition);
+    key.addEventListener("click", () => playSound({ key: key.getAttribute("data-key") }))
 });
+
