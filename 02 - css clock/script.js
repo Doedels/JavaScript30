778323b0058e62877b0e0957b898b81768470b23
Doedels <sandermarkies@yahoo.com>
2020-10-03 16:40:49 +0200

    refactor hour-hand

--- 02 - css clock/script.js
+++ 02 - css clock/script.js
@@ -6,6 +6,5 @@ setInterval(() => {
     const date = new Date();
     sec.style.transform = `rotate(${90 + date.getSeconds() * 6}deg)`;
     min.style.transform = `rotate(${90 + date.getMinutes() * 6}deg)`;
-    const twelveHours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
-    hour.style.transform = `rotate(${90 + twelveHours * 30}deg)`;
+    hour.style.transform = `rotate(${90 + date.getHours() * 30}deg)`;
 }, 1000);
